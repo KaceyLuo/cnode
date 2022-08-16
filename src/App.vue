9d5add2cdf971e71e7c5e8ae377ff7a59379015a
kaceyluo <luo1111111111111@gmail.com>
2022-08-16 18:25:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <router-view name="slidebar"></router-view>
+      <router-view name="main"></router-view>
+
+
+    </div>
+  </div>
+</template>
+<script>
+import Header from './components/Header.vue'
+import postList from './components/postList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    postList,
+  }
+}
+
+</script>
+
+
+<style lang="scss">
+.main {
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="header">
+        <router-link :to="{ name: 'root' }">
+            <img src="../assets/cnodejs_light.svg" alt="" sizes="" srcset="">
+
+        </router-link>
+        <ul>
+            <li><a href="#">首页</a></li>
+            <li><a href="https://cnodejs.org/getstart">新手入门</a></li>
+            <li><a href="https://cnodejs.org/api">API</a></li>
+            <li><a href="https://cnodejs.org/about">关于</a></li>
+            <li><a
+                    href="https://github.com/login/oauth/authorize?response_type=code&redirect_uri=https%3A%2F%2Fcnodejs.org%2Fauth%2Fgithub%2Fcallback&client_id=0625d398dd9166a196e9">注册</a>
+            </li>
+            <li><a href="https://cnodejs.org/signin">登录</a></li>
+
+
+
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+
+</script>
+<!-- scoped只对当前页面有效 -->
+<style scoped>
+.header {
+    background-color: #5a5555;
+    height: 50px;
+}
+
+img {
+    max-width: 120px;
+    margin-left: 50px;
+    margin-top: 10px;
+}
+
+ul {
+    list-style: none;
+    float: right;
+    margin: 4px;
+}
+
+li {
+    display: inline-block;
+    padding: 10px 15px;
+}
+
+a {
+    text-decoration: none;
+    color: #ccc;
+    font-size: 14px;
+    text-shadow: none;
+}
+</style>
